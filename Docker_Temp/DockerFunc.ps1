3731182d0cc6d034107e6bef141852b0194ef21e
avinash-chellapandian <52741185+avinash-chellapandian@users.noreply.github.com>
2020-09-01 11:57:06 +0530

    Update DockerFunc.ps1

--- Docker_Temp/DockerFunc.ps1
+++ Docker_Temp/DockerFunc.ps1
@@ -1,4 +1,8 @@
-﻿      param (
+function TestDockerFile {
+
+[CmdletBinding()]
+      param (
+      [parameter(mandatory=$true)]$DOTNETVERSION,
 	 [parameter(mandatory=$true)]$USERNAME,
 	[parameter(mandatory=$true)] $GROUP,
 	[parameter(mandatory=$true)]$DLLNAME
@@ -8,7 +12,9 @@ New-Item $env:temp\Kube-Generate -type Directory
 $sourceurl = "https://raw.githubusercontent.com/avinash-chellapandian/Template/master/Docker_Temp/Test.txt"
 $destpath = "$env:temp\Kube-Generate\Tempalate.txt"
 Invoke-WebRequest -Uri $sourceurl -OutFile $destpath
-$content = (Get-Content -Path $env:temp\Kube-Generate\Tempalate.txt).Replace('$USERNAME',"$USERNAME").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
-Remove-Item .\Deployment_new\ -Confirm:$true
-New-Item .\Deployment_new\ -ItemType Directory
-$content | Out-File -FilePath .\Deployment_new\Dockerfile
+$content = (Get-Content -Path $env:temp\Kube-Generate\Tempalate.txt).Replace('$DOTNETVERSION',"$DOTNETVERSION").Replace('$USERNAME',"$USERNAME").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
+Remove-Item .\Deployment\ -Confirm:$true
+New-Item .\Deployment\ -ItemType Directory
+$content | Out-File -FilePath .\Deployment\Dockerfile
+
+}
