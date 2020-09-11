c3193698248d7f3e6abafea9512531afacc9eb1c
avinash-chellapandian <52741185+avinash-chellapandian@users.noreply.github.com>
2020-09-11 10:21:02 +0530

    Update TestDockerFile.ps1

--- Docker_Temp/TestDockerFile.ps1
+++ Docker_Temp/TestDockerFile.ps1
@@ -1,14 +1,32 @@
-﻿      param (
-	 [parameter(mandatory=$true)]$USERNAME,
-	[parameter(mandatory=$true)] $GROUP,
-	[parameter(mandatory=$true)]$DLLNAME
+function TestDockerFile {
+
+[CmdletBinding()]
+      param (
+     [String] $DOTNETVERSION=$(Read-Host -prompt "Please enter the.NET Version as 3.1.7 or 2.1.21"),
+	 [String]$USERNAME=$(Read-Host -prompt "Please enter the username of the Service"),
+	[String]$GROUP=$(Read-Host -prompt "Please enter the Groupname of the Service"),
+	[String]$DLLNAME=$(Read-Host -prompt "Please enter the .dll name")
 )
 Remove-Item $env:temp\Kube-Generate -Confirm:$true 
 New-Item $env:temp\Kube-Generate -type Directory
-$sourceurl = "https://raw.githubusercontent.com/avinash-chellapandian/Template/master/Docker_Temp/Test.txt"
-$destpath = "$env:temp\Kube-Generate\Tempalate.txt"
+$sourceurl = "https://raw.githubusercontent.com/avinash-chellapandian/Template/master/Docker_Temp/Dockerfile"
+$destpath = "$env:temp\Kube-Generate\Dockerfile"
 Invoke-WebRequest -Uri $sourceurl -OutFile $destpath
-$content = (Get-Content -Path $env:temp\Kube-Generate\Tempalate.txt).Replace('$USERNAME',"$USERNAME").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
-Remove-Item .\Deployment_new\ -Confirm:$true
-New-Item .\Deployment_new\ -ItemType Directory
-$content | Out-File -FilePath .\Deployment_new\Dockerfile
+Remove-Item .\Deployment\ -Confirm:$true
+New-Item .\Deployment\ -ItemType Directory
+if($DOTNETVERSION -eq "3.1.7")
+{
+$content = (Get-Content -Path $env:temp\Kube-Generate\Dockerfile).Replace('$USERNAME',"$USERNAME").Replace('$IMAGETYPE',"3.1-bionic").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
+
+}
+elseif ($DOTNETVERSION -eq "2.1.21") {
+$content = (Get-Content -Path $env:temp\Kube-Generate\Dockerfile).Replace('$USERNAME',"$USERNAME").Replace('$IMAGETYPE',"2.1-bionic").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
+
+}
+else {
+$content = (Get-Content -Path $env:temp\Kube-Generate\Dockerfile).Replace('$USERNAME',"$USERNAME").Replace('$IMAGETYPE',"2.1-bionic").Replace('$GROUP',"$GROUP").Replace('$DLLNAME',"$DLLNAME").Replace('$Timestamp',$(Get-Date -Format F) +" "+'Version: 01')
+}
+$content | Out-File -encoding UTF8 -FilePath .\Deployment\Dockerfile
+Write-Host "Dockerfile created $DOTNETVERSION"
+
+}
